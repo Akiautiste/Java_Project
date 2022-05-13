6669027520600b6e63ffb4d00148846bb4905acc
Akiautiste <39983471+Akiautiste@users.noreply.github.com>
2022-05-13 20:25:17 +0800

    第二次提交

--- myStudentManager/src/com/itheima/test/StudentManager.java
+++ myStudentManager/src/com/itheima/test/StudentManager.java
@@ -17,7 +17,6 @@ public class StudentManager {
         lo:while(true){
             System.out.println("--------欢迎来到学生管理系统--------");
             System.out.println("1 添加学生");
-            System.out.println("1 添加学生");
             System.out.println("2 删除学生");
             System.out.println("3 修改学生");
             System.out.println("4 查看学生");
